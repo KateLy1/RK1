10231761b692df0099617143ec96e9c768a97306
KateLy <91429213+KateLy1@users.noreply.github.com>
2025-03-13 15:51:11 +0300

    Update sem1_task9.py

--- sem1_task9.py
+++ sem1_task9.py
@@ -1,3 +1,7 @@
+#Дан не отсортированный массив целых чисел. Необходимо перенести в начало 
+#массива все четные числа, сохраняя их очередность. То есть если 8 стояла после 2, 
+#то после переноса в начало, он по-прежнему должна стоять после 2
+
 def evenFirst(arr):
     evenIndex = 0
     for i in range(len(arr)):
@@ -6,3 +10,24 @@ def evenFirst(arr):
             evenIndex += 1
     return arr
 print(evenFirst(a))
+
+import unittest
+class TestGrades(unittest.TestCase):
+
+    def test1(self):
+        self.assertEqual(evenFirst([1, 1, 1, 1, 1, 1]), [1, 1, 1, 1, 1, 1])
+
+    def test2(self):
+        self.assertEqual(evenFirst([2, 4, 6, 8]), [2, 4, 6, 8])
+
+    def test3(self):
+        self.assertEqual(evenFirst([2]), [2])
+
+    def test4(self):
+        self.assertEqual(evenFirst([]), [])
+        
+    def test5(self):
+        self.assertEqual(evenFirst([3, 2, 4, 1, 11, 8, 9]), [2, 4, 8, 1, 11, 3, 9])
+
+if __name__ == '__main__':
+    unittest.main(argv=['first-arg-is-ignored'], exit=False)

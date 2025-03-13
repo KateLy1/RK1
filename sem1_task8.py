63c904c2d52043ced627f99e166ca56a8178f053
KateLy <91429213+KateLy1@users.noreply.github.com>
2025-03-13 15:42:00 +0300

    Update sem1_task8.py

--- sem1_task8.py
+++ sem1_task8.py
@@ -1,3 +1,6 @@
+#Дан массив состоящий из нулей, единиц и двоек.
+#Необходимо его отсортировать за линейное время
+
 def sortColors(nums):
     low = 0
     mid = 0
@@ -14,3 +17,30 @@ def sortColors(nums):
             high -= 1
     return nums
 print(sortColors(a))
+
+import unittest
+class TestGrades(unittest.TestCase):
+
+    def test1(self):
+        self.assertEqual(sortColors([1, 1, 1, 1, 1, 1]), [1, 1, 1, 1, 1, 1])
+
+    def test2(self):
+        self.assertEqual(sortColors([1, 2, 0, 0, 2, 0, 2, 0]), [0, 0, 0, 0, 1, 2, 2, 2])
+
+    def test3(self):
+        self.assertEqual(sortColors([0]), [0])
+
+    def test4(self):
+        self.assertEqual(sortColors([1, 1, 1, 1, 0, 0]), [0, 0, 1, 1, 1, 1])
+        
+    def test5(self):
+        self.assertEqual(sortColors([0, 0, 1, 1, 2, 2]), [0, 0, 1, 1, 2, 2])
+        
+    def test6(self):
+        self.assertEqual(sortColors([0, 1, 0, 1, 0, 0]), [0, 0, 0, 0, 1, 1])
+    
+    def test7(self):
+        self.assertEqual(sortColors([2, 2, 2, 0, 1, 0, 1, 0, 0]), [0, 0, 0, 0, 1, 1, 2, 2, 2])
+
+if __name__ == '__main__':
+    unittest.main(argv=['first-arg-is-ignored'], exit=False)

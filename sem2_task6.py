ddf320fcdbfe1438c74bcc1061c417ab98d1e14c
KateLy <91429213+KateLy1@users.noreply.github.com>
2025-03-13 16:20:39 +0300

    Update sem2_task6.py

--- sem2_task6.py
+++ sem2_task6.py
@@ -1,3 +1,6 @@
+#В исходную строку добавили некоторое количество символов. Необходимо выявить, была ли строка a исходной для строки b.
+#Метод указателей
+
 def isSubsequence(a, b):
     i = 0
     lenA = len(a)
@@ -10,4 +13,23 @@ def isSubsequence(a, b):
                 break
     return i == lenA
 
-print(isSubsequence(a, b))
+import unittest
+class TestIsSubsequence(unittest.TestCase):
+
+    def test1(self):
+        self.assertTrue(isSubsequence("", "abc"))
+
+    def test2(self):
+        self.assertTrue(isSubsequence("", ""))
+
+    def test3(self):
+        self.assertTrue(isSubsequence("abc", "ahbgdc"))
+
+    def test4(self):
+        self.assertFalse(isSubsequence("axc", "ahbgdc"))
+
+    def test5(self):
+        self.assertTrue(isSubsequence("abc", "abc"))
+        
+if __name__ == '__main__':
+    unittest.main(argv=['first-arg-is-ignored'], exit=False)

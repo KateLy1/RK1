1543bb9c0238de4f1e0b0873156cd3d7158745d4
KateLy <91429213+KateLy1@users.noreply.github.com>
2025-03-13 15:01:46 +0300

    Update sem1_task3.py

--- sem1_task3.py
+++ sem1_task3.py
@@ -1,3 +1,8 @@
+#Дан массив целых чисел.
+#Необходимо развернуть его.
+#Сделать это надо за линейное время без 
+дополнительных аллокаций памяти
+
 def reverseArray(array):
     left = 0
     right = len(array) - 1
@@ -7,3 +12,22 @@ def reverseArray(array):
         right -= 1
     return array
 print(reverseArray(a))
+
+import unittest
+
+class TestGrades(unittest.TestCase):
+
+    def test1(self):
+        self.assertEqual(reverseArray([1, 2, 3, 4, 5]), [5, 4, 3, 2, 1])
+
+    def test2(self):
+        self.assertEqual(reverseArray([1, 1, 1, 1]), [1, 1, 1, 1])
+
+    def test3(self):
+        self.assertEqual(reverseArray([0]), [0])
+
+    def test4(self):
+        self.assertEqual(reverseArray([-1, -2, -3, -4]), [-4, -3, -2, -1])
+
+if __name__ == '__main__':
+    unittest.main(argv=['first-arg-is-ignored'], exit=False)

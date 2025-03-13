fac704b01bdc2bc53c72e1929308b008fcb2bea1
KateLy <91429213+KateLy1@users.noreply.github.com>
2025-03-13 15:32:31 +0300

    Update sem1_task6.py

--- sem1_task6.py
+++ sem1_task6.py
@@ -1,3 +1,10 @@
+#Дано два отсортированных массива. Необходимо 
+#написать функцию которая объединит эти два массива 
+#в один отсортированный.
+#Первый массив имеет размер, равный 
+#результирующиму массиву, значения в конце равны 
+#нулям. Мы не должны создавать третий массив.
+
 def merge(arr1, arr2):
     pointer1 = len(arr1) - len(arr2) - 1
     pointer2 = len(arr2) - 1
@@ -12,3 +19,18 @@ def merge(arr1, arr2):
         pointer3 -= 1
     return arr1        
 print(merge(a, b))
+
+import unittest
+class TestGrades(unittest.TestCase):
+
+    def test1(self):
+        self.assertEqual(merge([1, 3, 5, 0, 0, 0], [2, 4, 6]), [1, 2, 3, 4, 5, 6])
+
+    def test2(self):
+        self.assertEqual(merged_sorted_array([1, 2, 3, 0, 0], [1, 4]), [1, 1, 2, 3, 4])
+
+    def test3(self):
+        self.assertEqual(merged_sorted_array([5, 0, 0, 0], [1, 2, 3]), [1, 2, 3, 5])
+
+if __name__ == '__main__':
+    unittest.main(argv=['first-arg-is-ignored'], exit=False)

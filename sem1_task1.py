c8997d62842467f896aed28a887811cab2306b60
KateLy <91429213+KateLy1@users.noreply.github.com>
2025-03-13 14:30:09 +0300

    Update sem1_task1.py

--- sem1_task1.py
+++ sem1_task1.py
@@ -1,8 +1,9 @@
+#Сумма чисел от 1 до 1000000
 def summ(array):
     s = 0
     for i in range(len(array)//2):
-        s += array[i] + array[len(array)-i-1]
+        s += array[i] + array[len(array)-i-1] #Сумма элементов с разных концов
     return s
 import numpy as np
 a = np.arange(1, 1000001, dtype=np.int64)
-print(summ(a))
+print(summ(a)) #Ожидаемый вывод: 500000500000

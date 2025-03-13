87b23a3e9ee648b7fc0e76a1d30168f2385faf81
KateLy <91429213+KateLy1@users.noreply.github.com>
2025-03-13 14:23:35 +0300

    Create sem2_task9.py

---
+++ sem2_task9.py
@@ -0,0 +1,12 @@
+def isPalindrome(s):
+    left = 0
+    right = len(s) - 1
+    while (left < right):
+        if (s[left] != s[right]):
+            return False
+        left += 1
+        right -= 1
+    return True
+
+c = "abac"
+print(isPalindrome(c))

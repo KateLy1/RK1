d08d405cdc219abea0a7dcc32ffe76f2e8f19e2f
KateLy <91429213+KateLy1@users.noreply.github.com>
2025-03-13 16:36:13 +0300

    Update sem2_task2.py

--- sem2_task2.py
+++ sem2_task2.py
@@ -1,3 +1,23 @@
+#Необходимо написать функцию, которая принимает на вход односвязный список и разворачивает его.
+
+class Node:
+    def __init__(self, data = None, next_Node = None):
+        self.data = data
+        self.next_Node = next_Node
+    def __str__(self):
+        return str(self.data)
+class Linkedlist:
+    head_Node = None
+    def __init__(self, head_Node):
+        self.head_Node = head_Node
+    def __str__(self):
+        nodes = []
+        current = self.head_Node
+        while current:
+            nodes.append(str(current.data))
+            current = current.next_Node
+        return " -> ".join(nodes)
+
 def reversedLinkedList(head):
     prev = None
     current = head
@@ -9,18 +29,40 @@ def reversedLinkedList(head):
     head = prev
     return head
 
-node1 = Node(10)
-node2 = Node(20)
-node3 = Node(30)
-node4 = Node(1)
-node5 = Node(3)
-node6 = Node(20)
-node1.next_Node = node2
-node2.next_Node = node3
-node3.next_Node = node4
-node4.next_Node = node5
-node5.next_Node = node6
-list_a = Linkedlist(node1)
-print(list_a)
-list_a.head_Node = reversedLinkedList(list_a.head_Node)
-print(list_a)
+import unittest
+
+def linked_list_to_list(head):
+    result = []
+    current = head
+    while current:
+        result.append(current.data)
+        current = current.next_Node
+    return result
+
+class TestReverseLinkedList(unittest.TestCase):
+
+    def test1(self):
+        self.assertIsNone(reversedLinkedList(None))
+
+    def test2(self):
+        head = Node(1)
+        reversed_head = reversedLinkedList(head)
+        self.assertEqual(linked_list_to_list(reversed_head), [1])
+
+    def test3(self):
+        head = Node(1, Node(2))
+        reversed_head = reversedLinkedList(head)
+        self.assertEqual(linked_list_to_list(reversed_head), [2, 1])
+
+    def test4(self):
+        head = Node(1, Node(2, Node(3)))
+        reversed_head = reversedLinkedList(head)
+        self.assertEqual(linked_list_to_list(reversed_head), [3, 2, 1])
+
+    def test5(self):
+        head = Node(1, Node(2, Node(3, Node(4))))
+        reversed_head = reversedLinkedList(head)
+        self.assertEqual(linked_list_to_list(reversed_head), [4, 3, 2, 1])
+        
+if __name__ == '__main__':
+    unittest.main(argv=['first-arg-is-ignored'], exit=False)

975a9482c2258ba1e4a4a9aeb10f24748e4f7442
KateLy <91429213+KateLy1@users.noreply.github.com>
2025-03-13 16:11:44 +0300

    Update sem2_task9.py

--- sem2_task9.py
+++ sem2_task9.py
@@ -1,3 +1,4 @@
+#Является ли строка палиндромом методом двух указателей
 def isPalindrome(s):
     left = 0
     right = len(s) - 1
@@ -8,5 +9,23 @@ def isPalindrome(s):
         right -= 1
     return True
 
-c = "abac"
-print(isPalindrome(c))
+import unittest
+class TestIsPalindrome(unittest.TestCase):
+
+    def test1(self):
+        self.assertTrue(isPalindrome(""))
+
+    def test2(self):
+        self.assertTrue(isPalindrome("a"))
+
+    def test3(self):
+        self.assertTrue(isPalindrome("aba"))
+
+    def test4(self):
+        self.assertTrue(isPalindrome("abba"))
+
+    def test5(self):
+        self.assertFalse(isPalindrome("abc"))
+
+if __name__ == '__main__':
+    unittest.main(argv=['first-arg-is-ignored'], exit=False)

57e8df88e885feb006c658bb8d6dfbb29d43162f
KateLy <91429213+KateLy1@users.noreply.github.com>
2025-03-13 15:38:40 +0300

    Update sem1_task7.py

--- sem1_task7.py
+++ sem1_task7.py
@@ -1,3 +1,8 @@
+#Дан массив, содержащий только 0 и 1. Напишите функцию, 
+#которая сортирует его так, чтобы все нули оказались в 
+#начале, а все единички - в конце. Решение должно быть 
+#in-place.
+
 def sort_binary_array(arr):
     left = 0
     right = len(arr) - 1
@@ -12,3 +17,27 @@ def sort_binary_array(arr):
             right -= 1
     return arr
 print(sort_binary_array(a))
+
+import unittest
+class TestGrades(unittest.TestCase):
+
+    def test1(self):
+        self.assertEqual(sort_binary_array([1, 1, 1, 1, 1, 1]), [1, 1, 1, 1, 1, 1])
+
+    def test2(self):
+        self.assertEqual(sort_binary_array([0, 0, 0, 0]), [0, 0, 0, 0])
+
+    def test3(self):
+        self.assertEqual(sort_binary_array([0]), [0])
+
+    def test4(self):
+        self.assertEqual(sort_binary_array([1, 1, 1, 1, 0, 0]), [0, 0, 1, 1, 1, 1])
+        
+    def test5(self):
+        self.assertEqual(sort_binary_array([0, 0, 1, 1, 1, 1]), [0, 0, 1, 1, 1, 1])
+        
+    def test6(self):
+        self.assertEqual(sort_binary_array([0, 1, 0, 1, 0, 0]), [0, 0, 0, 0, 1, 1])
+
+if __name__ == '__main__':
+    unittest.main(argv=['first-arg-is-ignored'], exit=False)

82177fc5ed731b5d468d31697d670f04b4fd9165
KateLy <91429213+KateLy1@users.noreply.github.com>
2025-03-13 15:21:10 +0300

    Update sem1_task5.py

--- sem1_task5.py
+++ sem1_task5.py
@@ -1,3 +1,7 @@
+# Дано два отсортированных массива. Необходимо 
+#написать функцию которая объединит эти два массива 
+#в один отсортированный.
+
 def merged_sorted_array(arr1, arr2):
     merged_array = []
     i = 0
@@ -13,3 +17,23 @@ def merged_sorted_array(arr1, arr2):
     merged_array.extend(arr2[j:])
     return merged_array
   print(merged_sorted_array(a, b))
+
+
+import unittest
+class TestGrades(unittest.TestCase):
+
+    def test1(self):
+        self.assertEqual(merged_sorted_array([], []), [])
+
+    def test2(self):
+        self.assertEqual(merged_sorted_array([1, 2, 3], []), [1, 2, 3])
+        self.assertEqual(merged_sorted_array([], [4, 5, 6]), [4, 5, 6])
+
+    def test3(self):
+        self.assertEqual(merged_sorted_array([1, 3, 5], [2, 4, 6]), [1, 2, 3, 4, 5, 6])
+
+    def test4(self):
+        self.assertEqual(merged_sorted_array([1, 2, 2, 3], [2, 3, 4, 4]), [1, 2, 2, 2, 3, 3, 4, 4])
+
+if __name__ == '__main__':
+    unittest.main(argv=['first-arg-is-ignored'], exit=False)
